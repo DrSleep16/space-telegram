4d6ee4fd916b2a604d12b1cc7eb82df12ae31618
Dmitry <Dr.Sleep1995@gmail.com>
2023-07-15 16:57:34 +0300

    publish photo to Telegram channel

--- spacebot.py
+++ spacebot.py
@@ -4,3 +4,4 @@ import telegram
 TOKEN = '6134575993:AAEFev1WHWIHSTlH4H8jA0JE0cLAY7UsoN4'
 bot = telegram.Bot(TOKEN)
 bot.sendMessage(chat_id='-1001709184129', text='Hello!')
+bot.sendPhoto(chat_id='-1001709184129',photo='https://www.simplilearn.com/ice9/free_resources_article_thumb/what_is_image_Processing.jpg')
