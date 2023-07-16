e100003f764e6e4eba17fb676afd9297b329a0c3
Dmitry <Dr.Sleep1995@gmail.com>
2023-07-16 21:16:21 +0300

    Обернул код в основном скрипте в ifmain

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from apod import download_apod_images
 from spacebot import publish_photos
 
 
-download_epic_images()
-get_latest_launch_photos()
-download_apod_images()
-publish_photos()
+if __name__ =='__main__':
+    download_epic_images()
+    get_latest_launch_photos()
+    download_apod_images()
+    publish_photos()
