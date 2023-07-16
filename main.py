a80e09ddd14c163c478c99a27283513969cc5480
Dmitry <Dr.Sleep1995@gmail.com>
2023-07-16 21:35:52 +0300

    Добавил переменные окружения в .env и их описание в README.md

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from epic import download_epic_images
 from fetch_spacex_images import get_latest_launch_photos
 from apod import download_apod_images
 from spacebot import publish_photos
+from dotenv import load_dotenv
 
 
 if __name__ =='__main__':
+    load_dotenv()
     download_epic_images()
     get_latest_launch_photos()
     download_apod_images()
