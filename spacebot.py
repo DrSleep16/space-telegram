9b7ddb61b9f453e414da1a9d58f253fa372ddbb7
Dmitry <Dr.Sleep1995@gmail.com>
2023-07-16 22:06:12 +0300

    Дал токенам специфические названия. Поправил README.md

--- spacebot.py
+++ spacebot.py
@@ -26,5 +26,5 @@ def publish_photos(directory='images/', channel_id=os.getenv('CHANNEL_ID'), inte
 if __name__ == '__main__':
     directory = 'images/'
     channel_id = os.getenv('TG_CHANNEL_ID')
-    interval_hours = int(os.getenv('INTERVAL_HOURS'))
+    interval_hours = int(os.getenv('PUBLISH_INTERVAL_HOURS'))
     publish_photos(directory, channel_id, interval_hours)
