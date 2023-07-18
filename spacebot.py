d75739ecb3e37ccb0fcfc933852c1490b95a812c
Dmitry <Dr.Sleep1995@gmail.com>
2023-07-18 14:35:10 +0300

    Исправил регистр переменных

--- spacebot.py
+++ spacebot.py
@@ -7,18 +7,17 @@ import time
 def publish_photos(channel_id, interval_hours, directory='images/'):
     photos = os.listdir(directory)
     random.shuffle(photos)
-
+    token = os.getenv('TG_TOKEN')
+    bot = telegram.Bot(token)
     while True:
         for photo in photos:
             photo_path = os.path.join(directory, photo)
             with open(photo_path, 'rb') as photo_file:
-                BOT.send_photo(chat_id=channel_id, photo=photo_file)
+                bot.send_photo(chat_id=channel_id, photo=photo_file)
             time.sleep(interval_hours * 3600)
 
 
 if __name__ == '__main__':
-    TOKEN = os.getenv('TG_TOKEN')
-    BOT = telegram.Bot(TOKEN)
     directory = 'images/'
     channel_id = os.getenv('TG_CHANNEL_ID')
     interval_hours = int(os.getenv('PUBLISH_INTERVAL_HOURS'))
